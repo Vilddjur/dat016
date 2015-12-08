1df98bf49db60db9e71e93bdc4ee41c5835966e3
oskar367 <oskar.jedvert@gmail.com>
2015-12-08 19:52:13 +0100

    added c files

---
+++ queue.c
@@ -0,0 +1,72 @@
+#include "queue.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Implementera interface från queue.h här */
+QueuePtr new_queue(){ 						// Skapar en ny (tom) kö
+  QueuePtr result = malloc(sizeof(struct QueueElement));
+  if(result == NULL) return NULL;
+
+  result->data = 0;
+  result->prio = 0;
+  result->next = NULL;
+
+  return result;
+}
+
+int size(QueuePtr q){
+  int size = 0;
+  QueuePtr temp = q;
+  while (temp->next) {
+    size++;
+    temp = temp->next;
+  }
+  return size;
+}
+
+void add(QueuePtr q, int prio, DataPtr d) {
+
+  QueuePtr meow = q;
+
+  QueuePtr res = malloc(sizeof(struct QueueElement));
+  res->prio = prio;
+  res->data = d;
+
+  while(meow->next && prio <= (meow->next->prio)){
+    meow = meow->next;
+  }
+  if(meow->next){
+    QueuePtr nextAfter = meow->next;
+    res->next = nextAfter;
+    meow->next = res;
+  }else{
+    meow->next = res;
+    res->next = 0;
+  }
+}
+
+DataPtr get_first(QueuePtr q){
+  return q->next->data;
+}
+
+void remove_first(QueuePtr q){
+  QueuePtr first = q->next;
+  if(first == NULL) return;
+  q->next = q->next->next;
+  free(first);
+}
+void clear(QueuePtr q){
+  QueuePtr next, deleteMe;
+  deleteMe = q; //HEAD
+  while(deleteMe){
+    next = deleteMe->next;
+    free(deleteMe);
+    deleteMe = next;
+  }
+  q->next = NULL;
+}
+void delete_queue(QueuePtr q){
+  clear(q);
+  free(q->next);
+  free(q);
+}
